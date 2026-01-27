2359b2e8611d98c63f41290cc15b954922af4512
Stanislav Svoboda <52444330+nuclearkofola@users.noreply.github.com>
2026-01-27 23:55:21 +0100

    User json test

---
+++ src/main.js
@@ -0,0 +1,55 @@
+class Users {
+  constructor(users) {
+    this.users = users;
+  }
+
+  printAll() {
+    for (let user of this.users) {
+      console.log(user);
+    }
+  }
+}
+
+
+let author = {
+  id: 1,
+  name: "Tomáš Palačinka",
+  username: "svalovec11",
+  email: "mozek@alfabeta.org"  ,
+  phone: "123456789",
+
+  get displayName() {
+    return `${this.name} (${this.username})`;
+  }
+};
+
+console.log(
+  author.displayName  
+  // Honza Kotrbek (svalovec11)
+);
+
+const usersList = document.querySelector(".users");
+
+if (usersList) {
+  const li = document.createElement("li");
+  li.textContent = author.displayName;
+  usersList.appendChild(li);
+}
+
+function doMath(times = 100,...rest) {
+  return rest.reduce( (times, sum, val) => sum + val, 0) * times; 
+
+}
+
+
+console.log(doMath( undefined,2,3));
+
+
+(async () => {
+
+  let response =  await fetch('https://jsonplaceholder.typicode.com/users');
+  let users = new Users(await response.json());
+  
+  users.printAll();
+})();
+
